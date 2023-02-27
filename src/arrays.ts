424f2fdac3c35f84325c69704b1ea817189fcd9a
Tyran Rice Jr <tyranjr@udel.edu>
2023-02-27 14:44:33 -0500

    Finished stringsToIntegers

--- src/arrays.ts
+++ src/arrays.ts
@@ -31,7 +31,10 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    const new_numbers = numbers.map((price: string): number =>
+        Number.isNaN(Number(price)) ? 0 : Number(price)
+    );
+    return new_numbers;
 }
 
 /**
