f33af028c4e596c5f8ea143ebbba22bc40a6f58a
Tyran Rice Jr <tyranjr@udel.edu>
2023-03-04 23:31:52 -0500

    Finished MakeAnswers function

--- src/nested.ts
+++ src/nested.ts
@@ -155,7 +155,16 @@ export function toCSV(questions: Question[]): string {
  * making the `text` an empty string, and using false for both `submitted` and `correct`.
  */
 export function makeAnswers(questions: Question[]): Answer[] {
-    return [];
+    const answers = [];
+    for (let i = 0; i < questions.length; i++) {
+        answers.push({
+            questionId: questions[i].id,
+            text: "",
+            submitted: false,
+            correct: false
+        });
+    }
+    return answers;
 }
 
 /***
