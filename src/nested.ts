0c154c04509979ae1734d0c62b4ea780c7fc240b
Tyran Rice Jr <tyranjr@udel.edu>
2023-03-04 23:51:37 -0500

    Finished addNewQuestion function

--- src/nested.ts
+++ src/nested.ts
@@ -1,5 +1,6 @@
 import { Answer } from "./interfaces/answer";
 import { Question, QuestionType } from "./interfaces/question";
+import { makeBlankQuestion } from "./objects";
 
 /**
  * Consumes an array of questions and returns a new array with only the questions
@@ -205,7 +206,12 @@ export function addNewQuestion(
     name: string,
     type: QuestionType
 ): Question[] {
-    return [];
+    const newArray = [];
+    for (let i = 0; i < questions.length; i++) {
+        newArray.push(questions[i]);
+    }
+    newArray.push(makeBlankQuestion(id, name, type));
+    return newArray;
 }
 
 /***
